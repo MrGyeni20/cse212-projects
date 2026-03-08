1afcea1c0455d9bc6586a217e4aa5a693f07035f
Nana Kwesi Gyeni <ngyeni@byupathway.edu>
2026-03-08 03:59:39 +0000

    Week 1 code assignment completed

--- week01/code/Arrays.cs
+++ week01/code/Arrays.cs
@@ -8,12 +8,20 @@ public static class Arrays
     /// <returns>array of doubles that are the multiples of the supplied number</returns>
     public static double[] MultiplesOf(double number, int length)
     {
-        // TODO Problem 1 Start
-        // Remember: Using comments in your program, write down your process for solving this problem
-        // step by step before you write the code. The plan should be clear enough that it could
-        // be implemented by another person.
+        // Step 1: Create a new array of doubles with size equal to 'length'
+        // Step 2: Loop through each index from 0 to length - 1
+        // Step 3: At each index i, calculate number * (i + 1) and store it in the array
+        //         (i + 1 because we want 1x, 2x, 3x... not 0x, 1x, 2x...)
+        // Step 4: Return the completed array
 
-        return []; // replace this return statement with your own
+        double[] result = new double[length];
+
+        for (int i = 0; i < length; i++)
+        {
+            result[i] = number * (i + 1);
+        }
+
+        return result;
     }
 
     /// <summary>
@@ -25,9 +33,23 @@ public static class Arrays
     /// </summary>
     public static void RotateListRight(List<int> data, int amount)
     {
-        // TODO Problem 2 Start
-        // Remember: Using comments in your program, write down your process for solving this problem
-        // step by step before you write the code. The plan should be clear enough that it could
-        // be implemented by another person.
+        // Step 1: Calculate the split point by subtracting amount from data.Count
+        //         Everything from splitPoint to the end moves to the front
+        //         Everything from 0 to splitPoint moves to the back
+        // Step 2: Get the last 'amount' items using GetRange(splitPoint, amount)
+        //         These will become the new beginning of the list
+        // Step 3: Get the first part using GetRange(0, splitPoint)
+        //         These will become the new end of the list
+        // Step 4: Clear the original list
+        // Step 5: Add the last part first, then add the first part after it
+
+        int splitPoint = data.Count - amount;
+
+        List<int> lastPart = data.GetRange(splitPoint, amount);
+        List<int> firstPart = data.GetRange(0, splitPoint);
+
+        data.Clear();
+        data.AddRange(lastPart);
+        data.AddRange(firstPart);
     }
 }
